761ec8e0f049ee52bc3ba456b6025d0b9ec0399e
ViniSouzaCosta <vinicius.souzacamargocosta@gmail.com>
2025-09-10 22:04:41 -0300

    auth finalizado

--- 05-autenticacao-com-jsonwebtoken/middleware/Auth.js
+++ 05-autenticacao-com-jsonwebtoken/middleware/Auth.js
@@ -5,9 +5,25 @@ import userController from '../controllers/userController.js';
 const Authorization = (req, res, next) => {
     const authToken = req.headers['authorization']
     if (authToken != undefined) {
-        next()
+        //Dividindo a string do token (para eliminar a palavra Berear)
+        const bearer = authToken.split(" ")
+        const token = bearer[1]
+        //Validando o token
+        jwt.verify(token, userController.JWTSecret,(error,data) => {
+            if(error){
+                res.status(401).json({error: "Token inválido"})
+            // TOKEN VÁLIDO    
+            }else{
+                req.token = token;
+                req.loggedUser ={
+                    id: data.id,
+                    email: data.email, 
+                };
+                next();
+            }
+        })
     } else{
-        res.status(401).json({error: "Token inválido"});
+        res.status(401).json({error: "Acesso não autorizado."});
     }
 };
 
